d8d18470e9d8292bf9a97427e2082cd7ba53af4d
SeoBeomSu <beom710@naver.com>
2026-02-04 15:03:59 +0900

    feat: feedback swagger 설명 추가

--- src/main/kotlin/kr/co/raildock/raildock_server/feedback/controller/FeedbackController.kt
+++ src/main/kotlin/kr/co/raildock/raildock_server/feedback/controller/FeedbackController.kt
@@ -1,5 +1,6 @@
 package kr.co.raildock.raildock_server.feedback.controller
 
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import kr.co.raildock.raildock_server.common.enum.ModelType
 import org.springframework.web.bind.annotation.*
@@ -10,6 +11,10 @@ import kr.co.raildock.raildock_server.feedback.dto.FeedbackUpdateRequest
 import kr.co.raildock.raildock_server.feedback.service.FeedbackService
 import org.springframework.web.multipart.MultipartFile
 
+@Tag(
+    name = "feedback",
+    description = "엔지니어 피드백 관리"
+)
 @RestController
 @RequestMapping("/feedback")
 class FeedbackController(
@@ -17,6 +22,7 @@ class FeedbackController(
 ) {
 
     @PostMapping(consumes = ["multipart/form-data"])
+    @Operation(summary = "피드백 작성 - problem ID와 Json파일 필요")
     fun create(
         @RequestParam problemId: UUID,
         @RequestParam jsonFile: MultipartFile
@@ -29,26 +35,31 @@ class FeedbackController(
         )
 
     @GetMapping
+    @Operation(summary = "피드백 리스트 조회")
     fun getList(): List<FeedbackResponse> =
         feedbackService.getList()
 
     @GetMapping("/{id}")
+    @Operation(summary = "피드백 id 조회")
     fun get(@PathVariable id: UUID): FeedbackResponse =
         feedbackService.get(id)
 
     @PatchMapping("/{id}")
+    @Operation(summary = "피드백 Update")
     fun update(
         @PathVariable id: UUID,
         @RequestBody request: FeedbackUpdateRequest
     ): FeedbackResponse =
         feedbackService.update(id, request)
 
-    @PostMapping("/{id}/complete")
+    @PatchMapping("/{id}/complete")
+    @Operation(summary = "피드백 상태변경용")
     fun complete(@PathVariable id: UUID) {
         feedbackService.complete(id)
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "피드백 단건 삭제용")
     fun delete(@PathVariable id: UUID) {
         feedbackService.delete(id)
     }
