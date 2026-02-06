0efc70570608ea1e54b6e6156d0d0e88afa41e7e
Huseong Yoon <huseongyoon@gmail.com>
2026-02-06 13:36:28 +0900

    feat: ChatbotClient

--- src/main/kotlin/kr/co/raildock/raildock_server/integration/llm/LlmClientImpl.kt
+++ src/main/kotlin/kr/co/raildock/raildock_server/integration/llm/LlmClientImpl.kt
@@ -1,9 +1,11 @@
 package kr.co.raildock.raildock_server.integration.llm
 
+import kr.co.raildock.raildock_server.integration.llm.chatbot.ChatbotClient
 import kr.co.raildock.raildock_server.integration.llm.pipeline.LlmPipelineClient
 import org.springframework.stereotype.Component
 
 @Component
 class LlmClientImpl(
-    override val pipeline: LlmPipelineClient
+    override val pipeline: LlmPipelineClient,
+    override val chatbot: ChatbotClient
 ): LlmClient
